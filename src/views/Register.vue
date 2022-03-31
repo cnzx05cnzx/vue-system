a4fba34bb07a43c983ec024604f0a84b7feb1611
cnzx05cnzx <2937204438@qq.com>
2022-03-31 20:28:34 +0800

    注册上传

---
+++ src/views/Register.vue
@@ -0,0 +1,148 @@
+<template>
+  <div class="login-wrap">
+    <div class="ms-login">
+      <div class="ms-title">基于属性商品分析平台</div>
+
+      <el-form :model="param" :rules="rules" ref="login" label-width="0px" class="ms-content">
+        <el-form-item prop="atype" style="text-align: center">
+
+        </el-form-item>
+
+        <el-form-item prop="username">
+          <el-input v-model="param.username" placeholder="请输入用户名">
+            <template #prepend>
+              <el-button icon="el-icon-user"></el-button>
+            </template>
+          </el-input>
+        </el-form-item>
+
+        <el-form-item prop="password">
+          <el-input
+              type="password"
+              placeholder="请输入密码"
+              v-model="param.password"
+          >
+            <template #prepend>
+              <el-button icon="el-icon-lock"></el-button>
+            </template>
+          </el-input>
+        </el-form-item>
+
+        <div class="login-btn">
+          <el-button type="primary" @click="regForm">注册</el-button>
+        </div>
+
+      </el-form>
+
+    </div>
+  </div>
+</template>
+
+<script>
+// import {reg} from "@/network/account"
+
+export default {
+  data() {
+    return {
+      param: {
+        username: "",
+        password: "",
+
+        roles: ['user', 'enterprise', 'admin'],
+      },
+      rules: {
+        username: [
+          {required: true, message: "请输入用户名", trigger: "blur"}
+        ],
+        password: [
+          {required: true, message: "请输入密码", trigger: "blur"}
+        ]
+      }
+    };
+  },
+  created() {
+    this.$store.commit("clearTags");
+  },
+  methods: {
+    regForm() {
+      this.$refs.login.validate(valid => {
+        if (valid) {
+          //数据模拟
+          this.$message.success("注册成功");
+          this.$router.push("/login")
+
+          //实际应用
+          // reg(this.param)
+          //     .then(res => {
+          //       console.log(res.result);
+          //       if(res.result===true)
+          //       {
+          //         this.$message.success("注册成功");
+          //         this.$router.push("/login")
+          //       }else{
+          //         this.$message.error("注册失败");
+          //       }
+          //
+          //     })
+          //     .catch(err => {
+          //       console.log(err);
+          //       this.$message.error("网络错误");
+          //       // return false;
+          //     })
+        }
+      });
+    }
+  },
+
+};
+</script>
+
+<style scoped>
+.login-wrap {
+  position: relative;
+  width: 100%;
+  height: 100%;
+  background-image: url(~@/assets/img/login-bg.jpg);
+  background-size: 100%;
+}
+
+.ms-title {
+  width: 100%;
+  line-height: 50px;
+  text-align: center;
+  font-size: 20px;
+  color: #728eff;
+  border-bottom: 1px solid #ddd;
+}
+
+.ms-login {
+  position: absolute;
+  left: 50%;
+  top: 45%;
+  width: 350px;
+  margin: -190px 0 0 -175px;
+  border-radius: 5px;
+  background: rgba(255, 255, 255, 0.3);
+  overflow: hidden;
+}
+
+.ms-content {
+  padding: 30px 30px;
+}
+
+.login-btn {
+  text-align: center;
+}
+
+.login-btn button {
+  width: 100%;
+  height: 36px;
+  margin-bottom: 10px;
+}
+
+.login-tips {
+  font-size: 12px;
+  line-height: 30px;
+  color: #fff;
+}
+</style>
